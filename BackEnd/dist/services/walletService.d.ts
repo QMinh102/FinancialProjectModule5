a8dd8cbd24d571450cd1238933c15f9888232e6c
quocm <quocminhhoang5004@gmail.com>
2023-06-01 13:53:16 +0700

    Add

--- BackEnd/dist/services/walletService.d.ts
+++ BackEnd/dist/services/walletService.d.ts
@@ -1,7 +1,7 @@
 declare class WalletService {
     private walletRepository;
     constructor();
-    getAll: (idUser: any) => Promise<void>;
+    getAll: (idUser: any) => Promise<any>;
     getOne: (id: any) => Promise<any>;
     create: (newWallet: any) => Promise<void>;
     update: (id: any, newWallet: any) => Promise<void>;
